9f6687582cbf5ac3ef0fc1e9104d3bb50d68ebea
José Vitor <josevitorbarroso123@gmail.com>
2026-02-17 14:01:57 -0300

    adiciona scripts `lint:check`, `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Conteúdo referente ao curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>o conhecimento te possibilita caminhar com os proprios pés 🧠</h1>
+  return <h1>o conhecimento te possibilita caminhar com os proprios pés 🧠</h1>;
 }
 
 export default Home;
