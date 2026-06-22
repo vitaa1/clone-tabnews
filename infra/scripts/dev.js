92aa9547f9f34c5d2862d57c5676cb7239b1658d
José Vitor <josevitorbarroso123@gmail.com>
2026-06-22 20:01:04 -0300

    feat: add `dev.js` in `infra/scripts` so that the container is stopped

---
+++ infra/scripts/dev.js
@@ -0,0 +1,34 @@
+const { spawn } = require("child_process");
+
+const isWindows = process.platform === "win32";
+
+function run(cmd, args = []) {
+  return new Promise((resolve) => {
+    spawn(cmd, args, { stdio: "inherit", shell: isWindows }).on(
+      "close",
+      (code) => resolve(code ?? 0),
+    );
+  });
+}
+
+async function main() {
+  await run("npm", ["run", "services:up"]);
+  await run("npm", ["run", "services:wait:database"]);
+  await run("npm", ["run", "migrations:up"]);
+
+  const proc = spawn("npm", ["run", "dev:next"], {
+    stdio: "inherit",
+    shell: isWindows,
+  });
+
+  const stop = () => run("npm", ["run", "services:stop"]);
+
+  process.on("SIGINT", () => proc.kill("SIGINT"));
+
+  proc.on("close", async (code) => {
+    await stop();
+    process.exit(code ?? 0);
+  });
+}
+
+main();
